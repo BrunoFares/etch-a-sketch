aaa923c27775c447745d0f43d0335786e5200297
BrunoFares <faresbruno04@gmail.com>
2023-07-27 02:20:40 +0300

    Worked on the hover effect of the mouse over the divs, still a few problems left to correct.

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
+const button = document.querySelector('button');
+let size;
+let oldSize;
 
-for(let i = 0; i < 16; i++) {
-    const outerDiv = document.createElement('div');
-    container.appendChild(outerDiv).setAttribute('class', 'row');
+button.addEventListener('click', createPad);
 
-    for(let j = 0; j < 16; j++) {
-        const innerDiv = document.createElement('div');
-        outerDiv.appendChild(innerDiv).setAttribute('class', `col${j+1}`);
+function createPad() {
+    oldSize = size;
+    size = +prompt("Choose the size of the pad you'd like to create:");
+
+    for(let i = 0; i < size; i++) {
+        const outerDiv = document.createElement('div');
+        container.appendChild(outerDiv).setAttribute('class', `row`);
+
+        for(let j = 0; j < size; j++) {
+            const innerDiv = document.createElement('div');
+            outerDiv.appendChild(innerDiv);
+        }
+    }
+
+    const individualDiv = document.querySelectorAll('div.row div');
+
+    for(let i = 0; i < size**2; i++) {
+        individualDiv[i].addEventListener('mouseover', function() {
+            individualDiv[i].style.cssText = "background-color: rgb(200, 0, 0);";
+        });
+    }
+
+    erasePad();
+}
+
+function erasePad() {
+    for(let i = 0; i < oldSize; i++) {
+        container.removeChild(container.firstElementChild);
     }
 }
