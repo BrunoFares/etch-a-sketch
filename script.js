4c5e38e516b0da21d80834744376cc8ed3744613
BrunoFares <faresbruno04@gmail.com>
2023-07-26 15:57:34 +0300

    Set up a grid-like structure of div elements.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const container = document.querySelector('#container')
+
 for(let i = 0; i < 16; i++) {
     const outerDiv = document.createElement('div');
-    document.body.appendChild(outerDiv).setAttribute('class', 'row');
+    container.appendChild(outerDiv).setAttribute('class', 'row');
 
     for(let j = 0; j < 16; j++) {
         const innerDiv = document.createElement('div');
